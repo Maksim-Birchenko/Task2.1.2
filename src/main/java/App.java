79d2b4bc5970b2eb4dd62d748a2982abd664d739
Maksim Birchenko <max.birchenko@mail.ru>
2023-10-10 18:32:17 +0300

    Create branch task_1,create class Cat,change class AppConfig,Change class App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,19 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        Cat cat=
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getCatName());
+        Cat cat2=
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2.getCatName());
+        boolean result = (bean == bean1);
+        System.out.println(result);
+        boolean result1 = (cat == cat2);
+        System.out.println(result1);
+
     }
 }
