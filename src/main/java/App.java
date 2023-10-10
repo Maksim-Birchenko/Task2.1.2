91ada2d628c4c7c1222d2ce2bf8869a15b564d46
Maksim Birchenko <max.birchenko@mail.ru>
2023-10-10 18:17:52 +0300

    Пустая задача

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getCat());
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getCat());
+        boolean result = bean1 == bean;
+        System.out.println(result);
+        boolean result1 = cat1 == cat;
+        System.out.println(result1);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Cat {
+    private String cat;
+
+    public String getCat() {
+        return cat;
+    }
+
+    public void setCat(String cat) {
+        this.cat = cat;
+    }
+}
